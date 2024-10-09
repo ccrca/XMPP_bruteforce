c3c0e2c68851dae3fd9f68be53eee14e23494d41
cacolac.py <cricoadrien@gmail.com>
2024-10-09 19:03:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,163 @@
+# XMPP Python Bruteforce - SASL SCRAM-SHA-1 Authentication 😁
+
+- [x] `Bruteforce with wordlist and random characters`
+- [x] `Wireshark Capture file`
+    - [ ] Check if there's other XMPP success in the capture and bruteforce them too 
+- [ ] Bruteforce and try to connect to the running server
+- [ ] Add another XMPP auth algorithm
+
+## Summary
+
+A simple python script to find a password from a XMPP authentication using SCRAM-SHA-1 method. You can add base64 encoded parameters one-by-one or simply put your pcap file in argument.
+This tool follow the TCP stream of a successful authentication challenge and try to correlate a password by encrypting it in the exact same way.
+
+You can enter a specific wordlist, a password or a "random prefix". If nothing is entered, by default, random passwords will be generated using random-prefix and a length of 3 printable characters (ascii letters, digits, punctuation and whitespace).
+
+
+c'est vite fait, au nom j'ai fait au mieux
+
+## Usage
+
+**Basic installation**
+```
+git clone https://github.com/ccrca/XMPP_bruteforce.git && cd XMPP_bruteforce
+
+pip install -r requirements.txt
+
+python3 xmpp_bruteforcer.py -u "user" -pf "v0X8v3Bz2T0CJGbJQyF0X+HI4Ts=" -w rockyou-75.txt --cnonce "fyko+d2lbbFgONRv9qkxdawL" --snonce "3rfcNHYJY1ZVvWVs7j" --salt "QSXCR+Q6sek8bf92" --server-sign "rmF9pqV8S7suAoZWja4dJRkFsKQ="
+```
+
+**With poetry**
+```
+git clone https://github.com/ccrca/XMPP_bruteforce.git && cd XMPP_bruteforce
+
+poetry install
+
+poetry run python3 xmpp_bruteforcer.py -u "user" -pf "v0X8v3Bz2T0CJGbJQyF0X+HI4Ts=" -w rockyou-75.txt --cnonce "fyko+d2lbbFgONRv9qkxdawL" --snonce "3rfcNHYJY1ZVvWVs7j" --salt "QSXCR+Q6sek8bf92" --server-sign "rmF9pqV8S7suAoZWja4dJRkFsKQ="
+
+```
+
+### Help !!
+
+```
+  _______________  _______  ___________           ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣰⣋⠉⠀⠀⠉⠉⣓⣦⣀⣀⠀⠀⠀⠀⠀⠀⠀
+ /  _____/\   _  \ \   _  \ \_   _____/__.__.     ⠀⠀⠀⠀⠀⠀⢠⡞⠛⠙⢻⣿⣷⣦⠀⠀⡖⠉⠉⢻⣿⣷⣂⠀⠀⠀⠀⠀
+/   \  ___/  /_\  \/  /_\  \ |    __)<   |  |     ⠀⠀⠀⠀⠀⠀⣼⡇⠀⢰⣾⡟⣿⣿⡆⢰⣷⣤⣴⡟⠛⢻⣿⠀⠀⠀⠀⠀
+\    \_\  \  \_/   \  \_/   \|     \  \___  |     ⠀⠀⠀⠀⠀⠀⡍⣿⣿⣿⣏⣀⣼⣿⠃⠘⠿⣿⣿⣧⣤⣼⡿⠧⠀⠀⠀⠀
+ \______  /\_____  /\_____  /\___  /  / ____|     ⠀⠀⠀⠀⠀⠠⣍⠖⠒⣒⠀⠀⠈⠀⠀⠀⠀⠈⠉⢨⡤⠄⠹⢺⠂⠀⠀⠀
+        \/       \/       \/     \/   \/          ⠀⠀⠀⠀⠀⠈⠛⢯⠁⢸⡳⡐⠦⠤⣤⠤⠤⡴⠘⢁⡇⠀⠀⡜⠁⠀⠀⠀
+           ____  ___                              ⠀⠀⠀⠀⠀⠀⠀⠀⢓⢼⡕⡗⠲⠬⣧⠤⠤⡗⠒⢋⣇⡠⠞⠁⠀⠀⠀⠀
+           \   \/  / _____ ______ ______ ______   ⠀⠀⠀⠀⠀⢀⡴⠚⠁⠀⠉⠙⠒⠒⠧⠤⠤⠧⠤⠚⠁⠳⣀⠀⠀⠀⠀⠀
+            \     / /     \\____ \\____ \\____ \  ⠀⢀⣠⣴⡼⢋⣀⡤⠼⠉⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⢆⠈⢦⡀⣠⢤⣄
+            /     \|  Y Y  \  |_> >  |_> >  |_> > ⠀⠘⣗⠥⡰⡏⠀⠀⠀⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⢙⡆⣉⢑⢻⠀
+           /___/\  \__|_|  /   __/|   __/|   __/  ⠀⠀⠀⠀⠛⠃⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠘⠛⠻⣼⠛⠀
+                 \_/     \/|__|   |__|   |__|     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢀⡤⣄⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+❯ python3 xmpp_bruteforcer.py -h
+usage: xmpp_bruteforcer.py [-h] [-cp CAPFILE] [-sh] [-p PASSWORD] [--random-suffix RANDOM_SUFFIX] [--prefix PREFIX]
+                           [--suffix SUFFIX] [-H HOST] [-P PORT] [-w WORDLIST] [-u USER] [-pf PROOF] [--cnonce CNONCE]
+                           [-i ITER] [--snonce SNONCE] [--salt B64_SALT] [-ss SERV_SIGN] [-v]
+
+XMPP Bruteforce si dieu veut
+
+options:
+  -h, --help               show this help message and exit
+
+Capture Options:
+  -cp CAPFILE, --capfile   Wireshark capture file
+  -sh, --show-packets      Show XMPP packets content
+
+Password Options:
+  -p PASSWORD, --password  Wanna try a specific password ?
+  --random-suffix          Generate password suffix
+  --prefix PREFIX          Known password prefix
+  --suffix SUFFIX          Known password suffix
+
+Server Options:
+  -H HOST, --host          XMPP server hostname or IP
+  -P PORT, --port          XMPP server port
+
+Wordlist Options:
+  -w WORDLIST, --wordlist  Wordlist
+
+User Options:
+  -u USER, --username      Username
+  -pf PROOF, --proof       Encoded proof
+
+Nonce and Salt Options:
+  --cnonce CNONCE          Client nonce
+  -i ITER, --iteration     Number of iterations
+  --snonce SNONCE          Server nonce
+  --salt B64_SALT          Salt used in challenge
+  -ss SERV_SIGN,           Server signature
+
+Verbosity Options:
+  -v, --verbose            Enable debug messages
+
+```
+
+If you want a better look at what's happenning, enable verbose mode `-v` `--verbose`
+```
+
+|█---------------------------------------| 3.99% Complete C= C= C=┌( `ー´)┘
+ -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+Tested password: pencil
+Salt : b'A%\xc2G\xe4:\xb1\xe9<m\xffv'
+Salted password : b'\x1d\x96\xee:R\x9bZ_\x9eG\xc0\x1f"\x9a,\xb8\xa6\xe1_}'
+Client key : b'\xe24\xc4{\xf6\xc3f\x96\xddm\x85+\x99\xaa\xa2\xba&UW('
+Stored key : e9d94660c39d65c38fbad91c358f14da0eef2bd6
+Auth message : n=user,r=fyko+d2lbbFgONRv9qkxdawL,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,s=QSXCR+Q6sek8bf92,i=4096,c=biws,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j
+Client Signature : b']q8\xc4\x86\xb0\xbf\xab\xdfI\xe3\xe2\xda\x8b\xd6\xe5\xc7\x9d\xb6\x13'
+Server Key : b'\x0f\xe0\x92X\xb3\xac\x85+\xa5\x02\xccb\xba\x90>\xaa\xcd\xbf}1'
+Server Signature : b'\xaea}\xa6\xa5|K\xbb.\x02\x86V\x8d\xae\x1d%\x19\x05\xb0\xa4'
+Tested Client Proof : bf45fcbf7073d93d022466c94321745fe1c8e13b
+Actual Client Proof : bf45fcbf7073d93d022466c94321745fe1c8e13b
+
+ (´-ω-`) Flag found : pencil !
+```
+
+The tool depends on tshark for the pcap parsing tasks. I recommend you to use it with other available options :wordlist, password, random-prefix
+Enable the `-sh` `--show-packets` option to get a better a view on the XML content that is extracted.
+```
+❯ python3 xmpp_bruteforcer.py -cp ./ch8.pcap -sh -p "pencil"
+Debug mode
+Using selector: EpollSelector
+Found 1 successful TCP streams.
+The First successful TCP stream number : 1
+----------------------------------------
+Packet 22 in TCP stream 1:
+xml_tag => <response xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>
+xml_cdata => biwsbj11c2VybmFtZSxyPWJHRnhkVzlwYkdGeGRXOXBiR0Z4ZFc5cENnPT0K
+ => </response>
+out => 1
+response => RESPONSE
+cdata is not base64 decodable :p
+cdata => n,,n=username,r=bGFxdW9pbGFxdW9pbGFxdW9pCg==
+----------------------------------------
+Packet 25 in TCP stream 1:
+xml_tag => <challenge xmlns='urn:ietf:params:xml:ns:xmpp-sasl'>
+xml_cdata => cj1iR0Z4ZFc5cGJHRnhkVzlwYkdGeGRXOXBDZz09YkdFZ2JXRnVieUJoSUdacGJta2djMkVnY0dWcGJtVUsscz1rTTZsVGpqblpXNEY4V0xib3lhZ2NBPT0saT00MDk2Cg==
+ => </challenge>
+in => 1
+challenge => CHALLENGE
+cdata => r=bGFxdW9pbGFxdW9pbGFxdW9pCg==bGEgbWFubyBhIGZpbmkgc2EgcGVpbmUK,s=kM6lTjjnZW4F8WLboyagcA==,i=4096
+----------------------------------------
+--- Parameters extracted ---
+ {'user': 'username', 'proof': 'b3VobGV6b3pvbHVpbGEK', 'cnonce': 'bGFxdW9pbGFxdW9pbGFxdW9pCg==', 'snonce': 'bGEgbWFubyBhIGZpbmkgc2EgcGVpbmUK', 'b64_salt': 'kM6lTjjnZW4F8WLboyagcA==', 'serv_sign': 'bmlxdWVsZXRhCg=='}
+--- -------------------- ---
+```
+
+## Resources
+
+- https://stackoverflow.com/questions/29298346/xmpp-sasl-scram-sha1-authentication
+- https://datatracker.ietf.org/doc/html/rfc5802
+- https://wiki.xmpp.org/web/SASL_Authentication_and_SCRAM
+
+## DISCLAIMER
+This tool is intended for educational and testing purposes only. The author of this tool is not responsible for any misuse or illegal activities performed with it. Use this tool only on systems you own or have explicit permission to test. Unauthorized access to computer systems is illegal and unethical.
+
+
+## 😼 Links
+
+[RootMe 💀](http://catleidoscope.sergethew.com/) - [HackTheBox 🟩](https://hackertyper.com/)
---
+++ xmpp_bruteforcer.py
@@ -0,0 +1,394 @@
+# SCRAM-SHA-1 decypher
+
+#  SaltedPassword  := Hi(Normalize(password), salt, i)
+#  ClientKey       := HMAC(SaltedPassword, "Client Key")
+#  StoredKey       := SHA1(ClientKey)
+#  AuthMessage     := client-first-message-bare + "," +             
+#                     server-first-message + "," +
+#                     client-final-message-without-proof
+#  ClientSignature := HMAC(StoredKey, AuthMessage)
+#  ClientProof     := ClientKey XOR ClientSignature
+#  ServerKey       := HMAC(SaltedPassword, "Server Key")
+#  ServerSignature := HMAC(ServerKey, AuthMessage)
+
+import argparse
+import base64
+import hashlib
+import hmac
+import re
+import pyshark
+from scapy.all import *
+import logging
+
+
+TIMEOUT=5
+# Define dictionnary stucture
+xml_dict = {
+        "user":None,
+        "proof":None,
+        "cnonce":None,
+        "snonce":None,
+        "b64_salt":None,
+        "serv_sign":None
+        }
+
+def display_banner():
+    banner = r"""
+  _______________  _______  ___________           ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣰⣋⠉⠀⠀⠉⠉⣓⣦⣀⣀⠀⠀⠀⠀⠀⠀⠀
+ /  _____/\   _  \ \   _  \ \_   _____/__.__.     ⠀⠀⠀⠀⠀⠀⢠⡞⠛⠙⢻⣿⣷⣦⠀⠀⡖⠉⠉⢻⣿⣷⣂⠀⠀⠀⠀⠀
+/   \  ___/  /_\  \/  /_\  \ |    __)<   |  |     ⠀⠀⠀⠀⠀⠀⣼⡇⠀⢰⣾⡟⣿⣿⡆⢰⣷⣤⣴⡟⠛⢻⣿⠀⠀⠀⠀⠀
+\    \_\  \  \_/   \  \_/   \|     \  \___  |     ⠀⠀⠀⠀⠀⠀⡍⣿⣿⣿⣏⣀⣼⣿⠃⠘⠿⣿⣿⣧⣤⣼⡿⠧⠀⠀⠀⠀
+ \______  /\_____  /\_____  /\___  /  / ____|     ⠀⠀⠀⠀⠀⠠⣍⠖⠒⣒⠀⠀⠈⠀⠀⠀⠀⠈⠉⢨⡤⠄⠹⢺⠂⠀⠀⠀
+        \/       \/       \/     \/   \/          ⠀⠀⠀⠀⠀⠈⠛⢯⠁⢸⡳⡐⠦⠤⣤⠤⠤⡴⠘⢁⡇⠀⠀⡜⠁⠀⠀⠀
+           ____  ___                              ⠀⠀⠀⠀⠀⠀⠀⠀⢓⢼⡕⡗⠲⠬⣧⠤⠤⡗⠒⢋⣇⡠⠞⠁⠀⠀⠀⠀
+           \   \/  / _____ ______ ______ ______   ⠀⠀⠀⠀⠀⢀⡴⠚⠁⠀⠉⠙⠒⠒⠧⠤⠤⠧⠤⠚⠁⠳⣀⠀⠀⠀⠀⠀
+            \     / /     \\____ \\____ \\____ \  ⠀⢀⣠⣴⡼⢋⣀⡤⠼⠉⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⢆⠈⢦⡀⣠⢤⣄
+            /     \|  Y Y  \  |_> >  |_> >  |_> > ⠀⠘⣗⠥⡰⡏⠀⠀⠀⠀⢧⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⢙⡆⣉⢑⢻⠀
+           /___/\  \__|_|  /   __/|   __/|   __/  ⠀⠀⠀⠀⠛⠃⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠘⠛⠻⣼⠛⠀
+                 \_/     \/|__|   |__|   |__|     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢀⡤⣄⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+    print(banner)
+
+def fast_progress_bar(current, total, bar_length=40):
+    percent = current / total
+    progress = int(bar_length * percent)
+    bar = '█' * progress + '-' * (bar_length - progress)
+    sys.stdout.write(f'\r|{bar}| {percent:.2%} Complete C= C= C=┌( `ー´)┘')
+    sys.stdout.flush()
+
+def ping_host(host, port):
+    # Check if port is open and unfiltered
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(TIMEOUT)
+        sock.connect((host, port))
+
+        print(f"Successfully connected to {host} on port {port}")
+        sock.close()
+
+    except socket.timeout:
+        print(f"Connection to {host} on port {port} timed out")
+    except socket.error as e:
+        print(f"Failed to connect to {host} on port {port}: {e}")
+
+    return
+
+###################### Parse capture with tshark ######################
+
+def parse_cap(capfile):
+    # Find pcap, parse XMPP, keep the TCP stream of the successfull challenge, parse client and server nonce
+    try:
+        pcap = pyshark.FileCapture(capfile, display_filter='xmpp')
+    except FileNotFoundError:
+        raise ValueError("Capture file not found, make sure you got the path right")
+    
+    xml_filter = ["_ws_expert_severity", "xml_attribute", "_ws_expert_group", "_ws_expert", "xmlns", "_ws_expert_message"]
+
+    successful_streams = find_successful_streams(pcap)
+    if successful_streams:
+        logging.debug(f"Found {len(successful_streams)} successful TCP streams.")
+        extract_auth_details(pcap, successful_streams, xml_filter)
+        if logging.DEBUG:
+            scram_print(xml_dict)
+    else:
+        logging.info("No successful TCP streams found.")
+
+def find_successful_streams(pcap):
+    successful_streams = []
+
+    for packet in pcap:
+        if 'XMPP' in packet:
+            xml_state = packet['XMPP'].get_field_value('success')
+            if xml_state == "SUCCESS":
+                tcp_stream = packet.tcp.stream
+                successful_streams.append(tcp_stream)
+    return successful_streams
+
+def extract_auth_details(pcap, successful_streams, xml_filter):
+
+    logging.info("\033[93mThe First successful TCP stream number : "+ successful_streams[0]+ "\033[0m")
+    successful_streams = successful_streams[0] # On verra plus tard hein
+
+    for packet in pcap:
+        if 'TCP' in packet and packet.tcp.stream in successful_streams:
+            if 'XMPP' in packet:
+                xmpp_layer = packet['XMPP']
+                logging.debug(f"\033[91mPacket {packet.number} in TCP stream {packet.tcp.stream}:\033[0m")
+
+                for data in xmpp_layer.field_names:
+                    if data in xml_filter:
+                        continue
+                    d = xmpp_layer.get_field_value(data)
+                    if data == "cdata":
+                        try:
+                            d = base64.b64decode(d).decode('utf-8')
+                            find_param(d, xmpp_layer)
+                        except Exception:
+                            logging.debug("cdata is not base64 decodable :p")
+
+                    logging.debug(f"{data} => {d}")
+                logging.debug("\033[93m-\033[0m" * 40)
+
+def find_param(cdata, xmpp_layer):
+    if xmpp_layer.get_field_value("challenge"): 
+    # Example : r=hydraFe3A1scL7C0jtKsm+kcg96MWg769FuRu,s=kM6lTjjnZW4F8WLboyagcA==,i=4096
+        xml_dict["b64_salt"] = re.search(r's=([^,]+)', cdata).group(1)
+
+    if xmpp_layer.get_field_value("response"):  
+    # Example : n,,n=koma_test,r=hydra | c=biws,r=hydraFe3A1scL7C0jtKsm+kcg96MWg769FuRu,p=mZU2Qekd8JR7ybCtb3hnJMGEfIg=
+        cnonce_match = re.search(r'n=([^,]+),r=([^,]+)', cdata)
+        if cnonce_match:
+            xml_dict["user"] = cnonce_match.group(1)
+            xml_dict["cnonce"] = cnonce_match.group(2)
+
+        snonce_match = re.search(r'r=([^,]*),p=', cdata)
+        if snonce_match:
+            xml_dict["snonce"] = snonce_match.group(1).strip(xml_dict["cnonce"])
+
+        xml_dict["proof"] = re.search(r'p=([^,]+)', cdata).group(1)
+
+    if xmpp_layer.get_field_value("success"):   
+    # Example : v=YQlegvbEwDo2o60YiK2iAkYyPKE=
+        xml_dict["serv_sign"] = cdata[2:]
+
+def scram_print(xmldata):
+    print("\033[92m--- Parameters extracted ---\033[0m")
+    print("\033[92m",xmldata,"\033[0m")
+    print("\033[92m--- -------------------- ---\033[0m")
+
+###################### End of capture parsing ######################
+
+def client_proof(client_key, client_sig):
+    #  ClientProof := ClientKey XOR ClientSignature
+    r = b""
+    for l in range(len(client_key)):
+        r += (client_key[l] ^ client_sig[l]).to_bytes(length=1, byteorder="big")
+    return r.hex()
+
+def get_auth_msg(user:str, salt:str, cnonce:str, snonce:str, iteration:int):
+    # AuthMessage := client-first-message-bare + "," + server-first-message + "," + client-final-message-without-proof
+    # no proof message => GS2 header + SCRAM username, nonce
+    auth_message = f"n={user},r={cnonce},r={cnonce}{snonce},s={salt},i={iteration},c=biws,r={cnonce}{snonce}"
+    return auth_message
+
+def get_client_signature(stored_key, auth_msg):
+    # ClientSignature := HMAC(StoredKey, AuthMessage)
+    sig = hmac.digest(bytes.fromhex(stored_key), auth_msg.encode(), hashlib.sha1)
+    return sig
+
+def get_server_signature(server_key, auth_msg):
+    # ServerSignature := HMAC(ServerKey, AuthMessage)
+    sig = hmac.digest(server_key, auth_msg.encode(), hashlib.sha1)
+    return sig
+
+def get_stored_key(client_key):
+    # StoredKey := SHA1(ClientKey)
+    h = hashlib.sha1(client_key).hexdigest()
+    return h
+
+def get_client_key(saltedpwd):
+    # ClientKey := HMAC(SaltedPassword, "Client Key")
+    key = hmac.digest(saltedpwd, b"Client Key", hashlib.sha1)
+    return key
+
+def get_server_key(saltedpwd):
+    # ServerKey := HMAC(SaltedPassword, "Server Key")
+    key = hmac.digest(saltedpwd, b"Server Key", hashlib.sha1)
+    return key
+
+def get_salted_pwd(pwd:str, salt:str, iter:int):
+    # SaltedPassword  := Hi(Normalize(password), salt, i)
+    salted_pwd = hashlib.pbkdf2_hmac('sha1', pwd.encode(), salt, int(iter))
+    return salted_pwd
+
+def get_salt(b64_salt):
+    try:
+        hex_salt = base64.b64decode(b64_salt).hex()
+        bytes_salt = bytes.fromhex(hex_salt)
+    except:
+        raise ValueError("Invalid salt encoding")
+    return bytes_salt
+
+def bruteforce(user:str="", proof:str="", b64_salt:str="", cnonce:str="", snonce:str="", password:str="",serv_sign:str="", 
+               wordlist:str="", prefix:str="", suffix:str="", random_suffix:int=0, capfile:str="", host:str="", port:int=0, iter:int=4096):
+
+    prefix = prefix if prefix is not None else ""
+    suffix = suffix if suffix is not None else ""
+    password = password if password is not None else ""
+    passwords = []
+    passwords.append(password)
+
+    if capfile is not None:
+        parse_cap(capfile)
+        if not xml_dict:
+            raise argparse.ArgumentTypeError("Couldn't extract anything from a successful challenge")
+
+        # idgaf code is trash, look end of code for an alternative
+        user = user if user is not None else xml_dict["user"]
+        proof = proof if proof is not None else xml_dict["proof"]
+        b64_salt = b64_salt if b64_salt is not None else xml_dict["b64_salt"]
+        cnonce = cnonce if cnonce is not None else xml_dict["cnonce"]
+        snonce = snonce if snonce is not None else xml_dict["snonce"]
+        serv_sign = serv_sign if serv_sign is not None else xml_dict["serv_sign"]
+
+        if any(value is None for value in xml_dict.values()):
+            raise ValueError("\033[91mMissing parameters to complete decryption.\n=> 'None' found in the dictionary.\033[0m")
+
+    if host is not None:
+        xmpp_pattern = r"/(?P<scheme>xmpp:\/\/)?(?P<host>[a-zA-Z0-9.-]+)(:(?P<port>[0-9]+))?"
+        xmpp_match = re.search(xmpp_pattern, host)
+        if xmpp_match:
+            host = xmpp_match.group("host")
+            port = port if port is not None else xmpp_match.group("port")
+        else:
+            raise ValueError("\033[91mInvalid host\033[0m")
+        logging.info("Host : " + host)
+        logging.info("Port : " + str(port))
+        ping_host(host, port)
+        # TRAvAUx EN COURS
+        return
+
+    if wordlist and os.path.isfile(wordlist):
+        try:
+            with open(wordlist, "r", encoding="utf-8") as w:
+                passwords.extend(w.readlines())
+        except UnicodeDecodeError:
+            print(f"\033[91mError: Unable to decode the file '{wordlist}' with utf-8 encoding.\033[0m")
+            try:
+                with open(wordlist, "r", encoding="latin-1") as w:
+                    passwords.extend(w.readlines())
+            except UnicodeDecodeError:
+                print(f"\033[91mError: Unable to decode the file '{wordlist}' with latin-1 encoding.\033[0m")
+
+    if random_suffix > 0:
+        # Load random characters
+        logging.info("Loading random prefixes with length : " + str(random_suffix))
+        charset = string.printable
+        for k in itertools.permutations(charset, random_suffix):
+            needle = "".join(k)
+            passwords.append(needle)
+
+    # Iterate over passwords
+    c = 0
+    for password in passwords:
+        c+=1
+        fast_progress_bar(c + 1, len(passwords))
+
+        if password is None or password == "":
+            continue  
+        logging.info("\n"+"\033[93m --\033[0m" * 25)
+
+        password = prefix+password.strip()+suffix
+        
+        logging.info("\033[93mTested password: \033[0m\x1b[43;1m"+ str(password)+"\033[0m") # ex: pencil
+        salt = get_salt(b64_salt)
+        logging.info("Salt : " + str(salt))
+        salted_pwd = get_salted_pwd(password, salt, iter)               # Normalize and salt tested password
+        logging.info("Salted password : "+ str(salted_pwd))
+        client_key = get_client_key(salted_pwd)
+        logging.info("Client key : "+ str(client_key))
+        storedkey = get_stored_key(client_key)
+        logging.info("Stored key : "+ str(storedkey))
+        auth_msg = get_auth_msg(user, b64_salt, cnonce, snonce, iter)
+        logging.info("Auth message : "+ str(auth_msg))
+        client_sig = get_client_signature(storedkey, auth_msg)
+        logging.info("Client Signature : "+ str(client_sig))
+        server_key = get_server_key(salted_pwd)
+        logging.info("Server Key : "+ str(server_key))
+        server_sig = get_server_signature(server_key, auth_msg)
+        logging.info("Server Signature : "+ str(server_sig))
+        tested_proof = client_proof(client_key, client_sig)
+        logging.info("Tested Client Proof : "+ str(tested_proof))
+
+        hexproof = base64.b64decode(proof).hex()
+        logging.info("Actual Client Proof : "+ str(hexproof))
+        if tested_proof == hexproof or server_sig == serv_sign:
+            print(f"\n\033[92m (´-ω-`) Flag found : {password} !\033[0m")
+            return
+    return print("\n\033[91m (｡•́︿•̀｡) Nothing found \033[0m")
+
+def file_path(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise print("\033[91m (｡•́︿•̀｡) Wordlist not found, make sure this file exists :", string,"\033[0m")
+
+# SCRAM-SHA-1 Decypher
+def main():
+    main_parser = argparse.ArgumentParser(description="XMPP Bruteforce si dieu veut")
+
+    # Groupe pour les fichiers de capture
+    capture_group = main_parser.add_argument_group('Capture Options')
+    capture_group.add_argument('-cp', '--capfile', required=False, type=file_path, help="Wireshark capture file")
+    capture_group.add_argument('-sh', '--show-packets', dest="showpkts", action="store_true", help="Show XMPP packets content", required=False)
+
+    # Groupe pour les options de mot de passe
+    password_group = main_parser.add_argument_group('Password Options')
+    password_group.add_argument('-p', '--password', required=False, help="Wanna try a specific password ?")
+    password_group.add_argument('--random-suffix', required=False, type=int, default=0, dest="random_suffix", help="Generate password suffix")
+    password_group.add_argument('--prefix', required=False, help="Known password prefix")
+    password_group.add_argument('--suffix', required=False, help="Known password suffix")
+
+    # Groupe pour les options de serveur
+    server_group = main_parser.add_argument_group('Server Options')
+    server_group.add_argument('-H', '--host', required=False, help="XMPP server hostname or IP")
+    server_group.add_argument('-P', '--port', type=int, default=5222, help="XMPP server port")
+
+    # Groupe pour les options de wordlist
+    wordlist_group = main_parser.add_argument_group('Wordlist Options')
+    wordlist_group.add_argument('-w', '--wordlist', required=False, type=file_path, help="Wordlist")
+
+    # Groupe pour les options d'utilisateur
+    user_group = main_parser.add_argument_group('User Options')
+    user_group.add_argument('-u', '--username', dest="user", required=False, help="Username")
+    user_group.add_argument('-pf', '--proof', required=False, help="Encoded proof")
+
+    # Groupe pour les options de nonce et de sel
+    nonce_group = main_parser.add_argument_group('Nonce and Salt Options')
+    nonce_group.add_argument('--cnonce', required=False, help="Client nonce")
+    nonce_group.add_argument('-i', '--iteration', dest="iter", type=int, default=4096, required=False, help="Number of iterations")
+    nonce_group.add_argument('--snonce', required=False, help="Server nonce")
+    nonce_group.add_argument('--salt', required=False, dest="b64_salt", help="Salt used in challenge")
+    nonce_group.add_argument('-ss', '--server-sign', dest="serv_sign", required=False, help="Server signature")
+
+    # Groupe pour les options de verbosité
+    verbosity_group = main_parser.add_argument_group('Verbosity Options')
+    verbosity_group.add_argument('-v', '--verbose', required=False, help="Enable debug messages", action="store_const", dest="loglevel", const=logging.INFO)
+
+    args = main_parser.parse_args()
+
+    if not args.wordlist and not args.password and not args.random_suffix:
+        logging.info("No parameter provided, testing with random 3 letters password")
+        args.random_suffix = 3
+
+    if args.showpkts:
+        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+        print("\033[2m\033[3mDebug mode\033[0m")
+    else:
+        logging.basicConfig(level=args.loglevel, format='%(message)s')
+
+    args_dict = vars(args)
+    args_dict.pop('loglevel', None)
+    args_dict.pop('showpkts', None)
+    bruteforce(**args_dict)
+
+if __name__ == "__main__":
+    main_parser = argparse.ArgumentParser(description="XMPP Bruteforce si dieu veut")
+    if len(sys.argv) == 1:
+        display_banner()
+        main_parser.print_help()
+        sys.exit(1)
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\033[91m\nI shall yield mine efforts to the twilight's embrace. (/=ω=)/\033[0m")
+        print("\033[2m\033[3mProgram interrupted\033[0m")
+
+    ############################################
+    # arguments = dict((key, value) for d in (locals(), xml_dict) for key, value in d.items())
+    # print("Required parameters, updated", required_params)
+    # for key in locals().keys():
+    #     if locals()[key] is None and key in required_params:
+    #         # logging.debug("Found empty " + key + " in xml extracted content")
+    #         locals()[key] = required_params[key]
+    # b64_salt = locals().get("b64_salt") # otherwise variable isn't really updated => naze
